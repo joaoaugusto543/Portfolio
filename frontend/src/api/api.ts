771da26013868c86e63fcc37a39a99b86c1ade1f
João Augusto <joaoaugustoclopes@gmail.com>
2024-04-30 21:43:07 -0300

    Add url api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api=axios.create({
-    baseURL:process.env.REACT_APP_API
+    baseURL:'https://portfolio-backend-five-rho.vercel.app/api'
 })
 
 export default api
